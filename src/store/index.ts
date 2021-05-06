4ea030ed369901c4f1b05a5055054b030d4e403e
zhongpei <1000@intry.cn>
2021-05-06 17:13:51 +0800

    0

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { state } from './state'
+import { state, IState } from './state'
+
+import { ActionContext } from 'vuex'
 
 Vue.use(Vuex)
-
+// { commit: Commit, state: State }
 export default new Vuex.Store({
     state,
     mutations: {},
     actions: {
-        setUserId({ state }, userId?: string) {
-            state.userId = userId
+        setUserId(context: ActionContext<IState, unknown>, userId?: string) {
+            context.state.userId = userId
         },
     },
     modules: {},
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,17 +2,24 @@
     <div class="about">
         <h1 @click="onClick">
             {{ title }}
+            <!-- {{ userId }} -->
         </h1>
     </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+// import { State, Getter, Action, Mutation, namespace } from 'vuex-class'
+
 import axios from 'axios'
+
 @Component({
     components: {},
 })
+@Component
 export default class About extends Vue {
     private title = 'About Page'
+    // @State('userId') userId
+
     onClick(): void {
         axios
             .post('/api/services/app/client/Time', null, {
